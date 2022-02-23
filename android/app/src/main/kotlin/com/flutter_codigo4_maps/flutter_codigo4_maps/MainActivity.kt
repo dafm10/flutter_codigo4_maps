477670efa2f76e65688490bffe5d0f3a994cda39
dafm10 <dafm.10@gmail.com>
2022-02-23 18:33:53 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/flutter_codigo4_maps/flutter_codigo4_maps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter_codigo4_maps.flutter_codigo4_maps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Google Maps"),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(0, 0),
+        ),
+      ),
+    );
+  }
+}
