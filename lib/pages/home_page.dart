14a0bf6a16cc2146b5476651e2c230d9d00c4518
dafm10 <dafm.10@gmail.com>
2022-02-23 18:55:24 -0500

    maps

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo4_maps/util/map_style.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +12,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  Map<MarkerId, Marker> _markers = {};
+
+  CameraPosition cameraPosition = CameraPosition(
+    target: LatLng(-8.059236, -79.052545),
+    zoom: 15,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +27,25 @@ class _HomePageState extends State<HomePage> {
         title: Text("Google Maps"),
       ),
       body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(0, 0),
-        ),
+        initialCameraPosition: cameraPosition,
+        mapType: MapType.normal,
+        onMapCreated: (GoogleMapController controller) {
+          controller.setMapStyle(jsonEncode(mapStyle));
+        },
+        // con .toSet() converie los marcadores
+        markers: _markers.values.toSet(),
+        onTap: (LatLng position){
+          MarkerId _markerId = MarkerId(_markers.length.toString());
+          // creamos un marcador
+          Marker _marker = Marker(
+              markerId: _markerId,
+            position: position,
+          );
+          _markers[_markerId] = _marker;
+          setState(() {
+
+          });
+        },
       ),
     );
   }
