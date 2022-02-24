210b5c66b883c160b1cc7cf948af1e38fcc42deb
dafm10 <dafm.10@gmail.com>
2022-02-23 20:51:14 -0500

    maps 6

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,7 +14,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Map<MarkerId, Marker> _markers = {};
+  final Map<MarkerId, Marker> _markers = {};
 
   /*CameraPosition cameraPosition = CameraPosition(
     target: LatLng(-8.059236, -79.052545),
@@ -34,7 +34,7 @@ class _HomePageState extends State<HomePage> {
 
   getPermission() async {
     PermissionStatus status = await Permission.location.status;
-    print(status);
+    //print(status);
   }
 
   Future<CameraPosition> initCurrentPosition() async {
@@ -50,51 +50,72 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Google Maps"),
+        title: const Text("Google Maps"),
       ),
       body: FutureBuilder(
         future: initCurrentPosition(),
-        builder: (BuildContext context, AsyncSnapshot snap){
-          if(snap.hasData){
-            return GoogleMap(
-              initialCameraPosition: cameraPosition,
-              myLocationEnabled: true,
-              myLocationButtonEnabled: true,
-              compassEnabled: true,
-              mapToolbarEnabled: true,
-              mapType: MapType.normal,
-              onMapCreated: (GoogleMapController controller) {
-                controller.setMapStyle(jsonEncode(mapStyle));
-              },
-              // con .toSet() converie los marcadores
-              // con values solo adquiero los valores del mapa
-              markers: _markers.values.toSet(),
-              onTap: (LatLng position) async {
-                final BitmapDescriptor _icon = await BitmapDescriptor.fromAssetImage(
-                    const ImageConfiguration(), 'assets/icons/fire.png');
-                MarkerId _markerId = MarkerId(_markers.length.toString());
-                // creamos un marcador
-                Marker _marker = Marker(
-                    markerId: _markerId,
-                    position: position,
-                    //icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
-                    //icon: BitmapDescriptor.defaultMarkerWithHue(200),
-                    icon: _icon,
-                    rotation: -10.0,
-                    // rotacuón del marcador
-                    draggable: true,
-                    onDragEnd: (LatLng newLocation) {
-                      //print(newLocation);
-                    },
-                    onTap: () {
-                      //print("Hola");
-                    });
-                _markers[_markerId] = _marker;
-                setState(() {});
-              },
+        builder: (BuildContext context, AsyncSnapshot snap) {
+          if (snap.hasData) {
+            return Stack(
+              children: [
+                GoogleMap(
+                  initialCameraPosition: cameraPosition,
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: true,
+                  compassEnabled: true,
+                  mapToolbarEnabled: true,
+                  mapType: MapType.normal,
+                  onMapCreated: (GoogleMapController controller) {
+                    controller.setMapStyle(jsonEncode(mapStyle));
+                  },
+                  // con .toSet() converie los marcadores
+                  // con values solo adquiero los valores del mapa
+                  markers: _markers.values.toSet(),
+                  onTap: (LatLng position) async {
+                    final BitmapDescriptor _icon =
+                    await BitmapDescriptor.fromAssetImage(
+                        const ImageConfiguration(), 'assets/icons/fire.png');
+                    MarkerId _markerId = MarkerId(_markers.length.toString());
+                    // creamos un marcador
+                    Marker _marker = Marker(
+                        markerId: _markerId,
+                        position: position,
+                        //icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
+                        //icon: BitmapDescriptor.defaultMarkerWithHue(200),
+                        icon: _icon,
+                        rotation: -10.0,
+                        // rotacuón del marcador
+                        draggable: true,
+                        onDragEnd: (LatLng newLocation) {
+                          //print(newLocation);
+                        },
+                        onTap: () {
+                          //print("Hola");
+                        });
+                    _markers[_markerId] = _marker;
+                    setState(() {});
+                  },
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 10.0),
+                    height: 50.0,
+                    width: double.infinity,
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+
+                      },
+                      icon: Icon(Icons.location_on),
+                      label: Text("My Location"),),
+                  ),
+                ),
+              ],
             );
           }
-          return Center(child: CircularProgressIndicator(),);
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
     );
